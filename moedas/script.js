4780a23d8ec588f79192fe0063ae18cd03db2e5b
Juliana Americo <juu.americo@gmail.com>
2021-09-18 18:32:27 -0300

    Conversor de moedas com bitcoin funcionando

--- moedas/script.js
+++ moedas/script.js
@@ -11,29 +11,47 @@ form.addEventListener("submit", async (e) => {
 })
 
 function fetchAPI(origem) {
-    let destino1 = "BRL"
-    let destino2 = "EUR"
-    let destino3 = "USD"
+    let destino1 = "EUR"
+    let destino2 = "USD"
 
-    if(origem === "BRL") destino1 = "BTC"
-    if(origem === "EUR") destino2 = "BTC"
-    if(origem === "USD") destino3 = "BTC"
+    let nomeOrigem = "Real Brasileiro"
+    if(origem === "USD") nomeOrigem = "Dólar Americano"
+    if(origem === "EUR") nomeOrigem = "Euro"
 
-    fetch(`https://economia.awesomeapi.com.br/last/${destino1}-${origem},${destino2}-${origem},${destino3}-${origem}`)
-    .then(res => res.json())
-    .then(dados => {
-        result.classList.remove("hide")
-        const valorDigitado = document.querySelector(`[data-value]`)
-        for(let i in dados) {
-            console.log(dados)
+    if(origem === "EUR") destino1 = "BRL"
+    if(origem === "USD") destino2 = "BRL"
 
-            const p = document.createElement("p")
-            p.classList.add("text")
-            p.style.marginTop = "1rem";
-            const resultado = (valorDigitado.value * dados[i].high).toFixed(2)
-            p.textContent = `${dados[i].name}: ${resultado}`
-            result.appendChild(p)
-        }
-    })
+    fetch(`https://economia.awesomeapi.com.br/last/${origem}-${destino1},${origem}-${destino2}`)
+        .then(res => res.json())
+        .then(dados => {
+            result.classList.remove("hide")
+            const valorDigitado = document.querySelector(`[data-value]`)
+            for(let i in dados) {
+                console.log(dados)
+
+                const p = document.createElement("p")
+                p.classList.add("text")
+                p.style.marginTop = "1rem";
+                const resultado = (valorDigitado.value * dados[i].high).toFixed(2)
+                p.textContent = `${dados[i].name}: ${resultado}`
+                result.appendChild(p)
+            }
+        })
+
+    fetch(`https://economia.awesomeapi.com.br/last/BTC-${origem}`)
+        .then(res => res.json())
+        .then(dados => {
+            result.classList.remove("hide")
+            const valorDigitado = document.querySelector(`[data-value]`)
+            for(let i in dados) {
+                console.log(dados)
+
+                const p = document.createElement("p")
+                p.classList.add("text")
+                p.style.marginTop = "1rem";
+                const resultado = (valorDigitado.value / dados[i].high).toFixed(2)
+                p.textContent = `Bitcoin/${nomeOrigem}: ${resultado}`
+                result.appendChild(p)
+            }
+        })
 }
-
