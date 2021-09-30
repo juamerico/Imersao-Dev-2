4371085cf997e29ef5287886771fe20b960e93a9
Juliana Americo <juu.americo@gmail.com>
2021-09-29 22:24:44 -0300

    Mentalista - Limite de tentativas

---
+++ aluraflix/script.js
@@ -0,0 +1,10 @@
+const result = document.querySelector(`[data-select='result']`)
+const input = document.querySelector(`[data-value]`)
+
+function printVideos() {
+    //print
+}
+
+function fetchAPI() {
+    fetch("http://")
+}
--- mentalista/script.js
+++ mentalista/script.js
@@ -1,34 +1,44 @@
 const input = document.querySelector(`[data-value]`)
-const secretNumber = Math.floor(Math.random() * 11)
-const result = document.querySelector(`[data-select='result']`)
+let secretNumber = Math.ceil(Math.random() * 10)
+let counter = 3
 
-function printResult(message) {
+const form = document.querySelector(`[data-form]`)
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let message = ""
+
+    if(input.value == secretNumber) {
+        message = `Parabéns, você acertou! O número secreto era ${secretNumber}!`
+        secretNumber = Math.ceil(Math.random() * 10)
+        counter = 3
+    } else if(input.value > secretNumber && counter !== 1) {
+        counter--
+        message = `Errou. O número secreto é menor. Resta(m) ${counter} tentativa(s).`
+    } else if(input.value < secretNumber && counter !== 1) {
+        counter--
+        message = `Errou. O número secreto é maior. Resta(m) ${counter} tentativa(s).`
+    } else {
+        message = `Não foi desta vez... O número secreto era ${secretNumber}. Tente novamente!`
+        secretNumber = Math.ceil(Math.random() * 10)
+        counter = 3
+    }
+
+    print(message)
+})
+
+function print(message) {
+    const result = document.querySelector(`[data-select='result']`)
     input.value = ""
     result.textContent = ""
-    
+
     result.classList.remove("hide")
     const p = document.createElement("p")
     p.classList.add("text")
 
     p.textContent = message
     result.appendChild(p)
+
+    console.log("Secret Number: " + secretNumber)
 }
-
-const form = document.querySelector(`[data-form]`)
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-
-    let message = ""
-    if(input.value > secretNumber) {
-        message = "Errou. O número secreto é menor."
-    } else if(input.value < secretNumber) {
-        message = "Errou. O número secreto é maior."
-    } else {
-        message = `Parabéns, você acertou! O número secreto era ${secretNumber}!`
-    }
-
-    printResult(message)
-})
 
 input.focus()
